260498986222de24a810ed548e4bd56bd738c25a
jmayordomom <jaimemayordomomoreno@gmail.com>
2024-03-22 12:40:39 +0100

    estructura de ficheros

---
+++ src/prod/tennisML.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Tennis Predict ML",
+    page_icon="🎾",
+)
+
+st.write("# Tennis Predict" )
+st.write("## Un modelo predictivo sobre el mundo del tenis 🎾")
+
+st.sidebar.success("Selecciona una de las páginas para navegar.")
